e062264466edc6ae6e8c0b7c408218e547402b08
mertbyd <mertbyd44@gmail.com>
2026-06-30 16:58:36 +0300

    feat: remove remaining Blazor/Web host scaffold projects

--- host/InventoryTrackingAutomation.Blazor.Server.Host/Menus/InventoryTrackingAutomationMenus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace InventoryTrackingAutomation.Blazor.Server.Host.Menus;
-
-public class InventoryTrackingAutomationMenus
-{
-    private const string Prefix = "InventoryTrackingAutomation";
-
-    //Add your menu items here...
-
-}
