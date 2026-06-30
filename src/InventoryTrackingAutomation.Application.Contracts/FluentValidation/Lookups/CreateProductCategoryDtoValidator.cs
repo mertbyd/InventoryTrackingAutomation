9116ae0500ea6d3201aff6a87d5b28b42989f179
mertbyd <mertbyd44@gmail.com>
2026-06-30 11:42:47 +0300

    Apply ExceptionCodes structure to all lookup validators

--- src/InventoryTrackingAutomation.Application.Contracts/FluentValidation/Lookups/CreateProductCategoryDtoValidator.cs
+++ src/InventoryTrackingAutomation.Application.Contracts/FluentValidation/Lookups/CreateProductCategoryDtoValidator.cs
@@ -1,20 +1,16 @@
 using FluentValidation;
 using InventoryTrackingAutomation.Dtos.Lookups;
+using InventoryTrackingAutomation.ExceptionCodes.ProductCategories;
 
 namespace InventoryTrackingAutomation.FluentValidation.Lookups;
 
-/// <summary>
-/// CreateProductCategoryDto için sade validation kuralları.
-/// </summary>
 public class CreateProductCategoryDtoValidator : AbstractValidator<CreateProductCategoryDto>
 {
     public CreateProductCategoryDtoValidator()
     {
-        RuleFor(x => x.Name)
-            .NotEmpty()
-            .MaximumLength(100);
-
-        RuleFor(x => x.Code)
-            .MaximumLength(50);
+        RuleFor(x => x.Code).NotEmpty().WithMessage(ProductCategoryExceptionCodes.ValidationExceptions.Code.CannotEmpty)
+                            .MaximumLength(50).WithMessage(ProductCategoryExceptionCodes.ValidationExceptions.Code.MaxLength);
+        RuleFor(x => x.Name).NotEmpty().WithMessage(ProductCategoryExceptionCodes.ValidationExceptions.Name.CannotEmpty)
+                            .MaximumLength(100).WithMessage(ProductCategoryExceptionCodes.ValidationExceptions.Name.MaxLength);
     }
 }
