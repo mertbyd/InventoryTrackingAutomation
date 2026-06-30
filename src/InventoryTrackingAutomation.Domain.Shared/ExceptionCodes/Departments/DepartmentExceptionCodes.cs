ee4fb6050e556b2dbd12af5107613f8d84c44b9a
mertbyd <mertbyd44@gmail.com>
2026-06-30 11:42:47 +0300

    Apply ExceptionCodes structure to all lookup validators

---
+++ src/InventoryTrackingAutomation.Domain.Shared/ExceptionCodes/Departments/DepartmentExceptionCodes.cs
@@ -0,0 +1,30 @@
+namespace InventoryTrackingAutomation.ExceptionCodes.Departments;
+
+public static class DepartmentExceptionCodes
+{
+    private const string DepartmentErrorCodesPrefix = $"LookupManagement.Department";
+    public const string NotFound = $"{DepartmentErrorCodesPrefix}:00001";
+    public const string AlreadyExists = $"{DepartmentErrorCodesPrefix}:00002";
+    
+    public static class ValidationExceptions
+    {
+        private const string DepartmentValidationExceptionsPrefix = $"{DepartmentErrorCodesPrefix}.ValidationExceptions";
+        
+        public static class Code
+        {
+            public const string CannotEmpty = $"{DepartmentValidationExceptionsPrefix}.Code:00001";
+            public const string MaxLength = $"{DepartmentValidationExceptionsPrefix}.Code:00002";
+        }
+
+        public static class Name
+        {
+            public const string CannotEmpty = $"{DepartmentValidationExceptionsPrefix}.Name:00001";
+            public const string MaxLength = $"{DepartmentValidationExceptionsPrefix}.Name:00002";
+        }
+
+        public static class Description
+        {
+            public const string MaxLength = $"{DepartmentValidationExceptionsPrefix}.Description:00001";
+        }
+    }
+}
