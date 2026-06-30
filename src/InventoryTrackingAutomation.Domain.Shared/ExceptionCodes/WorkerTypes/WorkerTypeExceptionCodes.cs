f5b3b26b08872d40bf197ff8626e5dba372c1eaa
mertbyd <mertbyd44@gmail.com>
2026-06-30 11:42:47 +0300

    Apply ExceptionCodes structure to all lookup validators

---
+++ src/InventoryTrackingAutomation.Domain.Shared/ExceptionCodes/WorkerTypes/WorkerTypeExceptionCodes.cs
@@ -0,0 +1,30 @@
+namespace InventoryTrackingAutomation.ExceptionCodes.WorkerTypes;
+
+public static class WorkerTypeExceptionCodes
+{
+    private const string WorkerTypeErrorCodesPrefix = $"LookupManagement.WorkerType";
+    public const string NotFound = $"{WorkerTypeErrorCodesPrefix}:00001";
+    public const string AlreadyExists = $"{WorkerTypeErrorCodesPrefix}:00002";
+    
+    public static class ValidationExceptions
+    {
+        private const string WorkerTypeValidationExceptionsPrefix = $"{WorkerTypeErrorCodesPrefix}.ValidationExceptions";
+        
+        public static class Code
+        {
+            public const string CannotEmpty = $"{WorkerTypeValidationExceptionsPrefix}.Code:00001";
+            public const string MaxLength = $"{WorkerTypeValidationExceptionsPrefix}.Code:00002";
+        }
+
+        public static class Name
+        {
+            public const string CannotEmpty = $"{WorkerTypeValidationExceptionsPrefix}.Name:00001";
+            public const string MaxLength = $"{WorkerTypeValidationExceptionsPrefix}.Name:00002";
+        }
+
+        public static class Description
+        {
+            public const string MaxLength = $"{WorkerTypeValidationExceptionsPrefix}.Description:00001";
+        }
+    }
+}
