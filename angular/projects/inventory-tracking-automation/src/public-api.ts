5a35ef70c70821734a68eb32c5cf16e8beeda301
mertbyd <mertbyd44@gmail.com>
2026-06-30 14:12:22 +0300

    feat: Remove Angular scaffold project

--- angular/projects/inventory-tracking-automation/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of inventory-tracking-automation
- */
-
-export * from './lib/components/inventory-tracking-automation.component';
-export * from './lib/services/inventory-tracking-automation.service';
-export * from './lib/services/inventory-notification-signalr.service';
-export * from './lib/inventory-tracking-automation.module';
