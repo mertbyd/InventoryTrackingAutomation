74343131b05c7e70ef1d1b405d23e9b26622a17c
mertbyd <mertbyd44@gmail.com>
2026-06-30 11:37:56 +0300

    Code review feedback applied (VehicleType exception codes replacing magic strings)

---
+++ src/InventoryTrackingAutomation.Domain.Shared/ExceptionCodes/VehicleTypes/VehicleTypeExceptionCodes.cs
@@ -0,0 +1,30 @@
+namespace InventoryTrackingAutomation.ExceptionCodes.VehicleTypes;
+
+public static class VehicleTypeExceptionCodes
+{
+    private const string VehicleTypeErrorCodesPrefix = $"VehicleManagement.VehicleType";
+    public const string NotFound = $"{VehicleTypeErrorCodesPrefix}:00001";
+    public const string AlreadyExists = $"{VehicleTypeErrorCodesPrefix}:00002";
+    
+    public static class ValidationExceptions
+    {
+        private const string VehicleTypeValidationExceptionsPrefix = $"{VehicleTypeErrorCodesPrefix}.ValidationExceptions";
+        
+        public static class Code
+        {
+            public const string CannotEmpty = $"{VehicleTypeValidationExceptionsPrefix}.Code:00001";
+            public const string MaxLength = $"{VehicleTypeValidationExceptionsPrefix}.Code:00002";
+        }
+
+        public static class Name
+        {
+            public const string CannotEmpty = $"{VehicleTypeValidationExceptionsPrefix}.Name:00001";
+            public const string MaxLength = $"{VehicleTypeValidationExceptionsPrefix}.Name:00002";
+        }
+
+        public static class Description
+        {
+            public const string MaxLength = $"{VehicleTypeValidationExceptionsPrefix}.Description:00001";
+        }
+    }
+}
